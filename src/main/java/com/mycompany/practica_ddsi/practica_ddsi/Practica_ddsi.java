3c0766c9b8a79eda1a293c849503fdf4cda3b120
Faprin <jmdominguezsegura@gmail.com>
2024-10-22 00:49:27 +0200

    segundo enunciado completo

--- src/main/java/com/mycompany/practica_ddsi/practica_ddsi/Practica_ddsi.java
+++ src/main/java/com/mycompany/practica_ddsi/practica_ddsi/Practica_ddsi.java
@@ -14,6 +14,7 @@ import Modelo.*;
 import java.awt.BorderLayout;
 import java.util.ArrayList;
 import java.util.FormatProcessor;
+import java.util.List;
 
 /**
  *
@@ -39,9 +40,11 @@ public class Practica_ddsi {
         System.out.println("11. Insertar un Socio");
         System.out.println("12. Eliminar un socio por DNI");
         System.out.println("13. Informacion de las actividades de la que es respondable un monitor (DNI)");
+        System.out.println("14. Actividades en las que esta inscrito un socio (DNI)");
+        System.out.println("15. Informacion de los socios inscritos en una actividad por nombre de la actividad");
         System.out.println("0. Salir ");
 
-        for (int i = 0; i < 3; i++);
+        for (int i = 0; i < 2; i++);
         System.out.println("\n");
 
         System.out.print(">>  ");
@@ -81,10 +84,14 @@ public class Practica_ddsi {
                         ArrayList<Socio> socios = (ArrayList<Socio>) query.getResultList();
 
                         for (Socio socio : socios) {
-                            System.out.println("Numero socio: " + socio.getNumeroSocio() + ", Nombre: " + socio.getNombre()
-                                    + ", DNI: " + socio.getDni() + ", Fecha de Nacimiento: " + socio.getFechaNacimiento()
-                                    + ", Telefono: " + socio.getTelefono() + ", Correo: " + socio.getCorreo()
-                                    + ", Fecha de Entrada: " + socio.getFechaEntrada() + ", Categoria: " + socio.getCategoria());
+                            System.out.println("Numero socio: " + socio.getNumeroSocio()
+                                    + ", Nombre: " + socio.getNombre()
+                                    + ", DNI: " + socio.getDni()
+                                    + ", Fecha de Nacimiento: " + socio.getFechaNacimiento()
+                                    + ", Telefono: " + socio.getTelefono()
+                                    + ", Correo: " + socio.getCorreo()
+                                    + ", Fecha de Entrada: " + socio.getFechaEntrada()
+                                    + ", Categoria: " + socio.getCategoria());
                         }
                         tr.commit();
 
@@ -108,10 +115,14 @@ public class Practica_ddsi {
                         ArrayList<Socio> socios = (ArrayList<Socio>) query.getResultList();
 
                         for (Socio socio : socios) {
-                            System.out.println("Numero socio: " + socio.getNumeroSocio() + ", Nombre: " + socio.getNombre()
-                                    + ", DNI: " + socio.getDni() + ", Fecha de Nacimiento: " + socio.getFechaNacimiento()
-                                    + ", Telefono: " + socio.getTelefono() + ", Correo: " + socio.getCorreo()
-                                    + ", Fecha de Entrada: " + socio.getFechaEntrada() + ", Categoria: " + socio.getCategoria());
+                            System.out.println("Numero socio: " + socio.getNumeroSocio()
+                                    + ", Nombre: " + socio.getNombre()
+                                    + ", DNI: " + socio.getDni()
+                                    + ", Fecha de Nacimiento: " + socio.getFechaNacimiento()
+                                    + ", Telefono: " + socio.getTelefono()
+                                    + ", Correo: " + socio.getCorreo()
+                                    + ", Fecha de Entrada: " + socio.getFechaEntrada()
+                                    + ", Categoria: " + socio.getCategoria());
                         }
                         tr.commit();
 
@@ -135,10 +146,14 @@ public class Practica_ddsi {
                         ArrayList<Socio> socios = (ArrayList<Socio>) query.getResultList();
 
                         for (Socio socio : socios) {
-                            System.out.println("Numero socio: " + socio.getNumeroSocio() + ", Nombre: " + socio.getNombre()
-                                    + ", DNI: " + socio.getDni() + ", Fecha de Nacimiento: " + socio.getFechaNacimiento()
-                                    + ", Telefono: " + socio.getTelefono() + ", Correo: " + socio.getCorreo()
-                                    + ", Fecha de Entrada: " + socio.getFechaEntrada() + ", Categoria: " + socio.getCategoria());
+                            System.out.println("Numero socio: " + socio.getNumeroSocio()
+                                    + ", Nombre: " + socio.getNombre()
+                                    + ", DNI: " + socio.getDni()
+                                    + ", Fecha de Nacimiento: " + socio.getFechaNacimiento()
+                                    + ", Telefono: " + socio.getTelefono()
+                                    + ", Correo: " + socio.getCorreo()
+                                    + ", Fecha de Entrada: " + socio.getFechaEntrada()
+                                    + ", Categoria: " + socio.getCategoria());
                         }
                         tr.commit();
 
@@ -243,10 +258,14 @@ public class Practica_ddsi {
                         Query query = session.createNamedQuery("Socio.findByNombre").setParameter("nombre", nombre);
                         Socio socio = (Socio) query.getSingleResult();
 
-                        System.out.println("Numero socio: " + socio.getNumeroSocio() + ", Nombre: " + socio.getNombre()
-                                + ", DNI: " + socio.getDni() + ", Fecha de Nacimiento: " + socio.getFechaNacimiento()
-                                + ", Telefono: " + socio.getTelefono() + ", Correo: " + socio.getCorreo()
-                                + ", Fecha de Entrada: " + socio.getFechaEntrada() + ", Categoria: " + socio.getCategoria());
+                        System.out.println("Numero socio: " + socio.getNumeroSocio()
+                                + ", Nombre: " + socio.getNombre()
+                                + ", DNI: " + socio.getDni()
+                                + ", Fecha de Nacimiento: " + socio.getFechaNacimiento()
+                                + ", Telefono: " + socio.getTelefono()
+                                + ", Correo: " + socio.getCorreo()
+                                + ", Fecha de Entrada: " + socio.getFechaEntrada()
+                                + ", Categoria: " + socio.getCategoria());
                         tr.commit();
 
                     } catch (Exception ex) {
@@ -274,10 +293,13 @@ public class Practica_ddsi {
                         ArrayList<Actividad> actividades = (ArrayList<Actividad>) query.getResultList();
 
                         for (Actividad actividad : actividades) {
-                            System.out.println("idActividad: " + actividad.getIdActividad() + ", Nombre: " + actividad.getNombre()
-                                    + ", Dia " + actividad.getDia() + ", Hora: " + actividad.getHora() + ", Descripcion: "
-                                    + actividad.getDescripcion() + ", Precio base: " + actividad.getPrecioBaseMes() + ", Monitor Responsable: "
-                                    + actividad.getMonitorResponsable());
+                            System.out.println("idActividad: " + actividad.getIdActividad()
+                                    + ", Nombre: " + actividad.getNombre()
+                                    + ", Dia " + actividad.getDia()
+                                    + ", Hora: " + actividad.getHora()
+                                    + ", Descripcion: " + actividad.getDescripcion()
+                                    + ", Precio base: " + actividad.getPrecioBaseMes()
+                                    + ", Monitor Responsable: " + actividad.getMonitorResponsable());
                         }
                         tr.commit();
 
@@ -304,10 +326,14 @@ public class Practica_ddsi {
                         ArrayList<Socio> socios = (ArrayList<Socio>) query.getResultList();
 
                         for (Socio socio : socios) {
-                            System.out.println("Numero socio: " + socio.getNumeroSocio() + ", Nombre: " + socio.getNombre()
-                                    + ", DNI: " + socio.getDni() + ", Fecha de Nacimiento: " + socio.getFechaNacimiento()
-                                    + ", Telefono: " + socio.getTelefono() + ", Correo: " + socio.getCorreo()
-                                    + ", Fecha de Entrada: " + socio.getFechaEntrada() + ", Categoria: " + socio.getCategoria());
+                            System.out.println("Numero socio: " + socio.getNumeroSocio()
+                                    + ", Nombre: " + socio.getNombre()
+                                    + ", DNI: " + socio.getDni()
+                                    + ", Fecha de Nacimiento: " + socio.getFechaNacimiento()
+                                    + ", Telefono: " + socio.getTelefono()
+                                    + ", Correo: " + socio.getCorreo()
+                                    + ", Fecha de Entrada: " + socio.getFechaEntrada()
+                                    + ", Categoria: " + socio.getCategoria());
                         }
 
                         tr.commit();
@@ -336,10 +362,14 @@ public class Practica_ddsi {
                         ArrayList<Socio> socios = (ArrayList<Socio>) query.getResultList();
 
                         for (Socio socio : socios) {
-                            System.out.println("Numero socio: " + socio.getNumeroSocio() + ", Nombre: " + socio.getNombre()
-                                    + ", DNI: " + socio.getDni() + ", Fecha de Nacimiento: " + socio.getFechaNacimiento()
-                                    + ", Telefono: " + socio.getTelefono() + ", Correo: " + socio.getCorreo()
-                                    + ", Fecha de Entrada: " + socio.getFechaEntrada() + ", Categoria: " + socio.getCategoria());
+                            System.out.println("Numero socio: " + socio.getNumeroSocio()
+                                    + ", Nombre: " + socio.getNombre()
+                                    + ", DNI: " + socio.getDni()
+                                    + ", Fecha de Nacimiento: " + socio.getFechaNacimiento()
+                                    + ", Telefono: " + socio.getTelefono()
+                                    + ", Correo: " + socio.getCorreo()
+                                    + ", Fecha de Entrada: " + socio.getFechaEntrada()
+                                    + ", Categoria: " + socio.getCategoria());
                         }
                         tr.commit();
                     } catch (Exception ex) {
@@ -424,25 +454,34 @@ public class Practica_ddsi {
 
                     break;
 
-                case 13:
+                case 13: // (suposiciones propias) relacion uno a muchos -> una actividad solo tiene un monitor responsable
+                    // -> un monitor puede ser responsable de una o muchas actividades
+                    // tiene un atributo la parte del  muchos correspondiente con la del uno
                     session = sessionFactory.openSession();
                     tr = session.beginTransaction();
 
                     System.out.print("DNI: ");
                     in.nextLine();
                     dni = in.nextLine();
-                    
+
                     try {
-                        Query query = session.createQuery("FROM Monitor m JOIN Actividad a WHERE a.monitorResponsable.dni=:dni").setParameter("dni", dni);
-                        ArrayList<Actividad> actividades = (ArrayList<Actividad>) query.getResultList();
-                        
-                        for(Actividad actividad : actividades){
-                            System.out.println("Nombre Actividad: " + actividad.getNombre() + ", dia: " + actividad.getDia() + ", hora: " + actividad.getHora()
-                                    + ", descripcion: " + actividad.getDescripcion() + ", precioBaseMes: " + actividad.getPrecioBaseMes() + 
-                                    ", Monitor Responsable: " + actividad.getMonitorResponsable());
+                        Query query = session.createQuery("FROM Actividad a WHERE a.monitorResponsable.dni=:dni").setParameter("dni", dni);
+                        List<Actividad> actividades = query.getResultList();
+
+                        if (!actividades.isEmpty()) {
+                            for (Actividad actividad : actividades) {
+                                System.out.println("Nombre Actividad: " + actividad.getNombre()
+                                        + ", dia: " + actividad.getDia()
+                                        + ", hora: " + actividad.getHora()
+                                        + ", descripcion: " + actividad.getDescripcion()
+                                        + ", precioBaseMes: " + actividad.getPrecioBaseMes()
+                                        + ", Monitor Responsable: " + actividad.getMonitorResponsable().getNombre());
+                            }
+                        } else {
+                            System.out.println("\nNo se ha encontrado ningun Monitor con el dni " + dni);
                         }
+
                         tr.commit();
-                        System.out.println("");
                     } catch (Exception e) {
                         tr.rollback();
                         System.out.println("No se ha podido recuperar la iformacion sobre la actividad que lleva a cabo el monitor con dni " + dni + " -> " + e.getMessage());
@@ -450,6 +489,76 @@ public class Practica_ddsi {
                         if (session != null && session.isOpen()) {
                             session.close();
                         }
+                    }
+                    break;
+
+                case 14: // necesario JOIN porque los dos son a muchos 
+                    session = sessionFactory.openSession();
+                    tr = session.beginTransaction();
+
+                    System.out.print("DNI: ");
+                    in.nextLine();
+                    dni = in.nextLine();
+
+                    try {
+                        Query query = session.createQuery("SELECT a FROM Actividad a JOIN a.socios s WHERE s.dni=:dni").setParameter("dni", dni);
+                        List<Actividad> actividades = query.getResultList();
+
+                        if (!actividades.isEmpty()) {
+                            for (Actividad actividad : actividades) {
+                                System.out.println("Nombre Actividad: " + actividad.getNombre()
+                                        + ", dia: " + actividad.getDia()
+                                        + ", hora: " + actividad.getHora()
+                                        + ", descripcion: " + actividad.getDescripcion()
+                                        + ", precioBaseMes: " + actividad.getPrecioBaseMes()
+                                        + ", Monitor Responsable: " + actividad.getMonitorResponsable().getNombre());
+                            }
+                        } else {
+                            System.out.println("No existen actividades para el socio con dni " + dni);
+                        }
+
+                    } catch (Exception e) {
+                        tr.rollback();
+                        System.out.println("No se han podido mostrar las actividades del socio con dni " + dni + ": " + e.getMessage());
+                    } finally {
+                        if (session != null && session.isOpen()) {
+                            session.close();
+                        }
+                    }
+                    break;
+
+                case 15: // Información de los socios inscritos en una actividad por nombre de la actividad
+                    session = sessionFactory.openSession();
+                    tr = session.beginTransaction();
+
+                    System.out.println("Nombre de la actividad: ");
+                    in.nextLine();
+                    String nomActividad = in.nextLine();
+
+                    try {
+                        Query query = session.createQuery("SELECT s FROM Socio s JOIN s.actividades a WHERE a.nombre=:nombre").setParameter("nombre", nomActividad);
+                        List<Socio> socios = query.getResultList();
+
+                        if (!socios.isEmpty()) {
+                            System.out.println(" ---- " + nomActividad + " ---- ");
+                            for (Socio socio : socios) {
+                                System.out.println("Numero socio: " + socio.getNumeroSocio()
+                                        + ", Nombre: " + socio.getNombre()
+                                        + ", DNI: " + socio.getDni()
+                                        + ", Fecha de Nacimiento: " + socio.getFechaNacimiento()
+                                        + ", Telefono: " + socio.getTelefono()
+                                        + ", Correo: " + socio.getCorreo()
+                                        + ", Fecha de Entrada: " + socio.getFechaEntrada()
+                                        + ", Categoria: " + socio.getCategoria());
+                            }
+                        }else {
+                            System.out.println("No se ha encontrado ninguna actividad con el nombre " + nomActividad);
+                        }
+                        
+                        tr.commit();
+                    } catch (Exception e) {
+                        tr.rollback();
+                        System.out.println("L");
                     }
                     break;
 
